3786f27c8860b4c1dc694d127d47dd10a0736976
MikhailWahib <mikhailwahib1@gmail.com>
2023-01-30 16:09:24 +0200

    Fully rebuilt with HTML and CSS

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
-const mobileMenuToggle = document.querySelector('#mobile-menu-toggle');
-const mobileMenu = document.querySelector('#mobile-menu');
-const mobileMenuBtns = document.querySelectorAll('#mobile-menu-btn');
-const mobileCatMenu = document.querySelectorAll('#mobile-cat-menu');
-
+const mobileMenuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".nav-content");
+const navLink = document.querySelectorAll(".nav-link");
+const mobileCatMenu = document.querySelectorAll(".cat-menu");
+const arrowIcon = document.querySelectorAll(".mobile-arrow");
 
 let show = false;
-mobileMenuToggle.addEventListener('click', () => {
+mobileMenuBtn.addEventListener("click", () => {
+  show = !show;
+  if (show) {
+    mobileMenu.style.display = "flex";
+    mobileMenuBtn.classList.add("close");
+  } else {
+    mobileMenu.style.display = "none";
+    mobileMenuBtn.classList.remove("close");
+  }
+});
+
+for (let i = 0; i < navLink.length; i++) {
+  let show = false;
+  navLink[i].addEventListener("click", () => {
     show = !show;
     if (show) {
-        mobileMenu.style.display = "block";
-    } else{
-        mobileMenu.style.display = "none";
-    }
-})
+      // reset other menus
+      mobileCatMenu.forEach((el) => {
+        el.style.display = "none";
+      });
+      arrowIcon.forEach((el) => {
+        el.style.transform = "rotate(0deg)";
+      });
 
-for (let i = 0; i < mobileMenuBtns.length; i++) {
-    let show = false;
-    mobileMenuBtns[i].addEventListener('click', () => {
-        show = !show;
-        if (show) {
-            mobileCatMenu[i].style.display = 'flex';
-        } else {
-            mobileCatMenu[i].style.display = 'none';
-        }
-    })
+      mobileCatMenu[i].style.display = "flex";
+      arrowIcon[i].style.transform = "rotate(-180deg)";
+    } else {
+      mobileCatMenu[i].style.display = "none";
+      arrowIcon[i].style.transform = "rotate(0deg)";
+    }
+  });
 }
